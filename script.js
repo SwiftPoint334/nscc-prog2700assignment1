df71e415a844a8e4f5a055b1e3f800dadd0257af
Parker Doggett <parker@doggett.one>
2025-01-28 15:39:00 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+const firstName = "Parker";
+const lastName = "Doggett";
+const age = 18;
+const student = true;
+const activities = ["swimming", "coding", "firefighting"];
+const address = {
+    street: "Hollingsworth",
+    city: "Bridgewater",
+    country: "Canada"
+};
+
+console.log("First Name: ", firstName);
+console.log("Last Name: ", lastName);
+console.log("Age: ", age);
+console.log("Is a student? ", student);
+console.log("Activities: ", activities);
+console.log("Address: ", address);
